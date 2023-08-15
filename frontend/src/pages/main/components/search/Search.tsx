b9c9de658ded62eca0e07da024c8f012a137118f
Kostaaa1 <kostaarsic123@gmail.com>
2023-08-15 23:51:07 +0200

    search fixed

--- frontend/src/pages/main/components/search/Search.tsx
+++ frontend/src/pages/main/components/search/Search.tsx
@@ -12,7 +12,6 @@ const Search = () => {
       searchRef.current &&
       !searchRef.current.contains(event.target as Node)
     ) {
-      console.log("run");
       setIsSearchFocused(false);
     }
   };
--- frontend/src/pages/main/components/search/SideSearch.tsx
+++ frontend/src/pages/main/components/search/SideSearch.tsx
@@ -1,22 +1,16 @@
-import { FC } from "react";
 import { motion } from "framer-motion";
 import Search from "./Search";
 
-interface SideSearchProps {
-  activeElement: string;
-}
-
-const SideSearch: FC<SideSearchProps> = () => {
+const SideSearch = () => {
   return (
     <motion.div
-      // className="fixed overflow-hidden z-10 top-0 left-[260px] h-full w-[400px] border-r rounded-3xl border-[#262626]"
-      className="fixed overflow-hidden z-10 top-0 left-[76px] h-full w-[400px] border-r rounded-3xl border-[#262626]"
+      className="fixed overflow-hidden z-10 top-0 left-[78px] h-full w-[420px] border-r border-[#262626] bg-[#000000]"
       initial={{ x: "-100%" }}
       animate={{ x: 0 }}
       exit={{ x: "-100%" }}
       transition={{ type: "spring", damping: 35, stiffness: 280 }}
     >
-      <div className="p-6 flex flex-col justify-between h-[150px] border-b border-[#262626]">
+      <div className="p-6 flex flex-col justify-between h-[140px] border-b border-[#262626]">
         <h1 className="text-3xl font-bold">Search</h1>
         <Search />
       </div>
