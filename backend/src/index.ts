4639d682a4efe53818fa67bfa086114b15ecfd39
Kostaaa1 <kostaarsic123@gmail.com>
2023-08-30 23:57:53 +0200

    removed socket for searching users

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,39 +5,62 @@ import supabase from "./db/supabase";
 
 export interface IUserData {
   username: string;
-  imageUrl: string;
+  image_url: string;
+}
+
+interface TMessage {
+  sender_id: string;
+  chatroom_id: string;
+  content: string;
+  created_at: string;
 }
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
-    origin: "http://localhost:5173", // Replace with your React app's URL
+    origin: "http://localhost:5173",
     methods: ["GET", "POST"],
   },
 });
 
 io.on("connection", (socket: any) => {
-  socket.on("users", async (input: any) => {
+  socket.on("join-room", (chatroomId: string) => {
+    console.log("Joined room", chatroomId);
+    socket.join(chatroomId);
+  });
+
+  socket.on("new-message", async (messageData: TMessage) => {
+    const { chatroom_id, content, sender_id, created_at } = messageData;
+    io.to(chatroom_id).emit("new-message", messageData);
     try {
-      socket.join(input);
-      console.log(input);
-      const { data: users, error } = await supabase
-        .from("users")
-        .select("*")
-        .ilike("username", `%${input}%`);
+      const { data, error: newMessageError } = await supabase
+        .from("messages")
+        .insert(messageData)
+        .select("created_at");
 
-      if (error) {
-        console.error("Supabase query error:", error.message);
-        return;
+      console.log(data?.[0]?.created_at);
+      const { error: lastMessageUpdateError } = await supabase
+        .from("chatrooms")
+        .update({
+          last_message: content,
+          created_at,
+        })
+        .eq("id", chatroom_id);
+
+      if (newMessageError) {
+        io.emit("Error occurred: ", newMessageError);
       }
-      console.log("Fetched users", users);
 
-      io.to(input).emit("users", users);
+      if (lastMessageUpdateError) {
+        io.emit("Error occurred: ", lastMessageUpdateError);
+      }
     } catch (error) {
-      console.error("Error occured while joining the room", error);
+      console.log(error);
+      io.emit("Error occurred", "An error occured in new-message socket");
     }
   });
+
 
   socket.on("disconnect", () => {
     console.log("A user disconnected");
